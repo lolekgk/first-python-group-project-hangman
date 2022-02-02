be18745e22f60fe20f173be34d2791b06b482b0a
lolekgk <karol.gajda97@gmail.com>
2022-02-02 21:38:02 +0100

    Added for loop in game_start().

--- hangman.py
+++ hangman.py
@@ -5,36 +5,26 @@ from pprint import pprint
 def ask_for_difficulty_lvl():
     difficulty_list = ['EASY', 'MEDIUM', 'HARD']
     while True:
-        difficulty_inp = input(f'Choose difficulty level from the list: {difficulty_list} ').upper()
+        difficulty_inp = input(f'Choose difficulty level from the list: {difficulty_list} \n').upper()
         if difficulty_inp in difficulty_list:
             return difficulty_inp
 
 
-
 def number_of_lives(difficulty_inp):
     if difficulty_inp == "EASY":
-        lives = 14
-        print(f"You choose EASY level with {lives} lives")
+        lives = 15
+        print(f"You choose EASY level with {lives} lives\n")
         return lives
     elif difficulty_inp == "MEDIUM":
-        lives = 12
-        print(f"You choose MEDIUM level with {lives} lives")
+        lives = 10
+        print(f"You choose MEDIUM level with {lives} lives\n")
         return lives
     elif difficulty_inp == "HARD":
-        lives = 10
-        print(f"You choose HARD level with {lives} lives")
+        lives = 5
+        print(f"You choose HARD level with {lives} lives\n")
         return lives
         
-# strip rozdziela kazda litere slowa
-def word_dashed(word_to_guess):
-    word_list = list(word_to_guess.strip(""))
-    word_list_encode = []
-    for i in word_list:
-        if i.isalpha():
-            word_list_encode.append(i.replace(i, "_")) 
-        else:
-            word_list_encode.append(i)
-    return print(" ".join(word_list_encode))
+
 
 def draw_a_word(difficulty_inp):
     f = open("countries-and-capitals.txt", "r")
@@ -44,7 +34,8 @@ def draw_a_word(difficulty_inp):
         result.append(x.split('|')[0])
    # w miejsce | wstawiany jest przecinek
     if difficulty_inp == "EASY":
-        word_to_guess = random.choice(result[:25])
+        word_to_guess = random.choice(result[19:21])
+        #sprawdzenie slowa - usunac pozniej printa
         print(word_to_guess)
         return word_to_guess
     elif difficulty_inp == "MEDIUM":
@@ -64,48 +55,61 @@ def is_a_letter(letter):
         else:
             return False
 
-already_tried_letters = []
-guessed_letters = []
 
-def ask_for_a_letter():
-    letter = input('Please provide a letter or type a quit if you want to exit: ').lower()
+def ask_for_a_letter(already_tried_letters):
+    letter = input('Please provide a letter or type a quit if you want to exit: \n').lower()
     while True:
         if is_a_letter(letter):
             if letter in already_tried_letters: #sprawdza czy letter jest w zbiorze już użytych liter, jeśli tak to zwraca że już ją użyto
-                print(f"You already used {letter}.")
                 print("Already used letters: ", (" ".join(already_tried_letters)))
+                print(f"You already used {letter}.\n")
                 return letter
             else:
                 already_tried_letters.append(letter) # jeśli litery nie użyto wcześniej, dodaje ją do listy already_tried_letters i zwraca całą listę
-                print("Already used letters: ", (" ".join(already_tried_letters)))
+                print("Already used letters: \n", (" ".join(already_tried_letters)))
                 break
-
         elif letter.upper() == 'QUIT':
             end_game = "See you soon!"
             eg = requests.get(f'http://artii.herokuapp.com/make?text={end_game}')
             print(eg.text)
-            return None
+            return 'end'
         else:
             if len(letter) == 0:
-                print("You did not provide any letter!")
+                print("You did not provide any letter!\n")
                 break
             else:
-                print(f"Wrong you provied '{letter}'! You need to provide one letter!")
+                print(f"Wrong you provied '{letter}'! You need to provide one letter!\n")
                 break
     return letter 
-    
-def guessing(word_to_guess):
+
+# strip rozdziela kazda litere slowa
+# zwraca odgadniete słowo zamienione na podłogi
+def word_dashed(word_to_guess):
+    word_list = list(word_to_guess.strip(""))
+    word_list_encode = []
+    for i in word_list:
+        # jezeli element jest literą, zamieniamy go na podloge, w przeciwnym razie - element jest pusty
+        # wstawiany jest pusty element
+        if i.isalpha():
+            word_list_encode.append(i.replace(i, "_")) 
+        else:
+            word_list_encode.append(i)
+    return print(" ".join(word_list_encode))
+#zwraca wyprintowane podlogi
+
+# podmienia podlogi na odgadniete litery
+def guessing(word_to_guess, already_tried_letters):
     word_list = list(word_to_guess.strip("").lower())
     word_list_encode = []
     if word_list[-1]== " ":
         word_list.pop()
     for i in word_list:
         if i in already_tried_letters or not i.isalpha():
-            word_list_encode.append(i)
+            word_list_encode.append(i) #zostaje to co jest, gdy i nie jest literą, lub jest w uzytym zbiorze
         else:
-            word_list_encode.append(i.replace(i, "_"))
+            word_list_encode.append(i.replace(i, "_")) #w przeciwnym razie zwracany jest uzupelniony
     return print(" ".join(word_list_encode))
-        
+
 # STEP 6
 # if the letter is present in the word iterate through all the letters in the variable
 # word_to_guess. If that letter is present in the already_tried_letters then display it,
@@ -131,55 +135,55 @@ def game_start():
     game_menu = "Welcome to Hangman"
     r = requests.get(f'http://artii.herokuapp.com/make?text={game_menu}')
     print(r.text)
-    difficulty = ask_for_difficulty_lvl() #pozniej mozna dodac, ze po zgadnieciu slowa znowu wybieramy poziom trudnosci
+    already_tried_letters = []
+    guessed_letters = []
+    difficulty = ask_for_difficulty_lvl() #pozniej mozna dodac, ze po zgadnieciu slowa znowu wybieramy poziom trudnosci   
     secret_word = draw_a_word(difficulty)
+    # secret_word = "Test"
     lives = number_of_lives(difficulty)
     word_dashed(secret_word)
-        # ten zbior nie wiem czy tutaj, czy w osobnej funkcji
-        #already_tried_letters = []
-        #to podobnie
-        #users_word = []
-        
-    while True: #pyta o literę a potem odpala funkcję guessing, która daną literkę wypisuje w haśle i potem pyta ponownie o literę / trzeba będzie przerwać gdy będzie całe hasło odgadnięte lub wszystkie stacone życia
-        a = ask_for_a_letter()
-        guessing(secret_word)  
-        secret_list = list(secret_word.strip("").lower())
-        if secret_list[-1]== " ":
-            secret_list.pop()
+#pyta o literę, a potem odpala funkcję guessing, która daną literkę wypisuje w haśle i potem pyta ponownie o literę 
+# / trzeba będzie przerwać gdy będzie całe hasło odgadnięte lub wszystkie stacone życia
+    while True: 
+        a = ask_for_a_letter(already_tried_letters)
+        guessing(secret_word, already_tried_letters)  
+        secret_list = list(secret_word.strip("").lower()) #lista stworzona z oddzielonych liter zgadniętego słowa
+        if a == 'end': #zakonczenie programu w przypadku wpisania quit
+            break
+        secret_list.sort()
+
+        for x in secret_list:
+            secret_list.pop(0)
+            if secret_list[0] != ' ':
+                break
+        # if secret_list[0]== " ":
+        #     secret_list.pop(0)
+        # if secret_list[0]== " ":
+        #     secret_list.pop(0)
         while True:
-            if a not in secret_list and is_a_letter(a) == True:
-                if a not in already_tried_letters:
-                    lives -= 1    
-                    warning = "You missed! Left lives: "
-                    l = requests.get(f'http://artii.herokuapp.com/make?text={warning}{lives}')
-                    print(l.text)
-                    print(f"You missed! Left lives: {lives}")
-                    break
+            if a not in secret_list and is_a_letter(a) == True: # jezeli litera nie znajduje sie w liscie slowa do odgadniecia i litera jest literą pojedyncza
+                lives -= 1    
+                warning = "You missed! Left lives: "
+                l = requests.get(f'http://artii.herokuapp.com/make?text={warning}{lives}')
+                print(l.text)
+                print(f"You missed! Left lives: {lives}")
+                break
             elif a in secret_list:  
                 warning = "You are correct! Left lives: "
                 l = requests.get(f'http://artii.herokuapp.com/make?text={warning}{lives}')
                 print(l.text)
                 guessed_letters.append(a)
-
-            break
+                break
+            #break
+        #tutaj mozna dodać warunek z rozpoczęciem gry po wygranej/przegranej
+    
         if set(guessed_letters) == set(secret_list):
-            print("Congratulations! You win!")
+            print("Congratulations! You win!\n")
             break
         elif lives == 0:
-            print("You lost!")
+            print("You lost!\n")
             break
-        # wy przypadku, gdy dlugosc slowa wybranego przez program nie jest podanemu przez nas
-        #bedziemy wciaz pytani o slowo
-        # while len(secret_word) != len(users_word):
-        #     ask = ask_for_a_letter(already_tried_letters)
-        
-        #konczymy dzialanie programu w przypadku podania quit - funkcja zwraca wtedy none == false
-        if a == False:
-                break
-    
-        # break
-    # lives = xxx
-    # l = requests.get(f'http://artii.herokuapp.com/make?text={lives}')
+
 
 if __name__ == "__main__":
     game_start()
